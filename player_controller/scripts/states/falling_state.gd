17d7f85009161a9ed704de047df04db2e50a49a3
fallendynasty <33785667+fallendynasty@users.noreply.github.com>
2025-02-14 00:30:04 +0800

    added gun,hp,made the code nicer, added acceleration to movement and friction

--- player_controller/scripts/states/falling_state.gd
+++ player_controller/scripts/states/falling_state.gd
@@ -10,39 +10,20 @@ func update(_delta: float) -> void:
 
 ## Called by the state machine on the engine's physics update tick.
 func physics_update(_delta: float) -> void:
-	# apply gravity if still falling
-	if not player.is_on_floor():
-		player.velocity.y -= player.GRAVITY * _delta
-		player.move_and_slide()
-		prints("[FallingState] Player is falling with velocity.y:", player.velocity.y)
-		return
+	player.velocity.y -= player.GRAVITY * _delta
+	super.handle_movement() # handle air movements (air strafing and stuff)
 
-	prints("[FallingState] Player has landed")
-	# handle air movements (air strafing and stuff)
-	var input_dir := Input.get_vector(player.INPUT_LEFT, player.INPUT_RIGHT, player.INPUT_FORWARD, player.INPUT_BACKWARD)
-	var move_dir := (player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if move_dir:
-		player.velocity.x = move_dir.x * player.MOVE_SPEED
-		player.velocity.z = move_dir.z * player.MOVE_SPEED
-	else:
-		player.velocity.x = move_toward(player.velocity.x, 0, player.MOVE_SPEED)
-		player.velocity.z = move_toward(player.velocity.z, 0, player.MOVE_SPEED)
-	player.move_and_slide()
+	if player.velocity.y == 0: # player on floor, check velocity => idle or running
+		var horizontal_velocity = Vector2(player.velocity.x, player.velocity.z).length()
+		if horizontal_velocity > 0:
+			finished.emit("RunningState")
+		else:
+			finished.emit("IdleState")
 
-	# player on floor, check velocity => idle or running
-	var horizontal_velocity = Vector2(player.velocity.x, player.velocity.z).length()
-	prints("[FallingState] horizontal_velocity:", horizontal_velocity)
-	if horizontal_velocity > 0:
-		finished.emit("RunningState")
-	else:
-		finished.emit("IdleState")
-
-## Called by the state machine upon changing the active state. The `data` parameter
-## is a dictionary with arbitrary data the state can use to initialize itself.
+## Called by the state machine upon changing the active state. The `data` parameter is a dictionary with arbitrary data the state can use to initialize itself.
 func enter(previous_state_path: String, data := {}) -> void:
 	prints("[FallingState] Falling State Entered")
 
-## Called by the state machine before changing the active state. Use this function
-## to clean up the state.
+## Called by the state machine before changing the active state. Use this function to clean up the state.
 func exit() -> void:
 	prints("[FallingState] Falling State Exited")
--- player_controller/scripts/states/state.gd
+++ player_controller/scripts/states/state.gd
@@ -18,12 +18,35 @@ func update(_delta: float) -> void:
 func physics_update(_delta: float) -> void:
 	pass
 
-## Called by the state machine upon changing the active state. The `data` parameter
-## is a dictionary with arbitrary data the state can use to initialize itself.
+## Called by the state machine upon changing the active state. The `data` parameter is a dictionary with arbitrary data the state can use to initialize itself.
 func enter(previous_state_path: String, data := {}) -> void:
 	pass
 
-## Called by the state machine before changing the active state. Use this function
-## to clean up the state.
+## Called by the state machine before changing the active state. Use this function to clean up the state.
 func exit() -> void:
 	pass
+
+# handling movement for all states
+# a bit janky
+func handle_movement() -> void:
+	var input_dir := Input.get_vector(player.INPUT_LEFT, player.INPUT_RIGHT, player.INPUT_FORWARD, player.INPUT_BACKWARD)
+	var move_dir := (player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	player.velocity.x += move_dir.x * player.MOVE_ACCELERATION 
+	player.velocity.z += move_dir.z * player.MOVE_ACCELERATION 
+	
+	#cap the max speed and slow down if the directional movement is not being pressed
+	if abs(player.velocity.x) > player.MAX_SPEED or move_dir.x == 0:
+		player.velocity.x = move_toward(player.velocity.x, 0, player.FRICTION)
+	if abs(player.velocity.z) > player.MAX_SPEED or move_dir.z == 0:
+		player.velocity.z = move_toward(player.velocity.z, 0, player.FRICTION)
+		
+	player.move_and_slide()
+	#print(player.velocity.x, '       ', player.velocity.z, '      ', player.velocity.length())
+	#print(move_dir, 'move_dir')
+	
+# handling signal 'finished' emission for running and idle 
+func handle_GroundedStates_signal_emission() -> void: 
+	if not player.is_on_floor():
+		finished.emit("FallingState")
+	elif Input.is_action_just_pressed(player.INPUT_JUMP):
+		finished.emit("JumpingState")
