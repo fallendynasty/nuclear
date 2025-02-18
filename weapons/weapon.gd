0d2d82c8859eb7523f67855145f49debbdff82a7
Yik Wee Koh <77543794+Yik-Wee@users.noreply.github.com>
2025-02-18 19:23:07 +0800

    add basic weapon methods and attributes

--- weapons/weapon.gd
+++ weapons/weapon.gd
@@ -1,11 +1,33 @@
+## Weapon base class for melee weapons / guns
 class_name Weapon extends Node
 
+## amount of ammo currently in the gun. Defaults to `-1` for infinity
+var _ammo_count: int
+## amount of ammo in each cartridge (number of bullets fired before reload). Defaults to `-1` for infinity
+var _ammo_per_cartridge: int
+## amount of damage the weapon deals
+var _damage: float
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _init(ammo_count: int = -1, ammo_per_cartridge: int = -1, damage: float = 0) -> void:
+	_ammo_count = ammo_count
+	_ammo_per_cartridge = ammo_per_cartridge
+	_damage = damage
 
+## amount of ammo currently in the gun. Defaults to `-1` for infinity
+func get_ammo_count() -> int:
+	return _ammo_count
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+## amount of ammo in each cartridge (number of bullets fired before reload). Defaults to `-1` for infinity
+func get_ammo_per_cartridge() -> int:
+	return _ammo_per_cartridge
+
+func get_damage() -> float:
+	return _damage
+
+## shoots the gun / hits with melee weapon
+func attack() -> void:
+	pass
+
+## reload the gun
+func reload() -> void:
 	pass
